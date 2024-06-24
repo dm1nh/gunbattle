0a977f0ca3677a32eb9373ae852f27eda6991182
dangminhngo <dangminhngo.dev@gmail.com>
2024-06-24 13:24:36 +0700

    fix: only allow get weapon if body is a player

--- scenes/boxes/weapons/weapon_box.gd
+++ scenes/boxes/weapons/weapon_box.gd
@@ -4,13 +4,15 @@ class_name WeaponBox
 var weapon: Type.Weapon
 var primary: bool = true
 var body_inside: bool = false
+var body_node: Node2D
 signal wait_get_box(box: Area2D, weapon: Type.Weapon, primary: bool)
 
 func _process(_delta):
-	if body_inside:
+	if body_inside and body_node is Player:
 		wait_get_box.emit(self, weapon, primary)
 
-func _on_body_entered(_body:Node2D):
+func _on_body_entered(body:Node2D):
+	body_node = body 
 	body_inside = true
 
 func _on_body_exited(_body:Node2D):
