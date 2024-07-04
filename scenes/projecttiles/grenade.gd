64ddd3b14e6afe15bbc265a1c1ced653199c0a78
dm1nh <dangminhngo.dev@gmail.com>
2024-07-04 19:22:58 +0700

    fix: fix grenade damage

--- scenes/projecttiles/grenade.gd
+++ scenes/projecttiles/grenade.gd
@@ -1,10 +1,10 @@
 extends RigidBody2D
 
 @export var speed: int = 300
-const DAMAGE_RADIUS: int = 48
+const DAMAGE_RADIUS: int = 64
 var exploded: int = false
-const GRENADE_MAX_DAMAGE: int = 100
-const GRENADE_MIN_DAMAGE: int = 50
+const GRENADE_MAX_DAMAGE: int = 120
+const GRENADE_MIN_DAMAGE: int = 40
 
 func _ready():
 	$Explosion.visible = false
@@ -12,8 +12,9 @@ func _ready():
 func _process(_delta):
 	if exploded:
 		for player in get_tree().get_nodes_in_group("players"):
-			var distance_from_grenade = player.global_position.distance_to(global_position)
+			var distance_from_grenade: float = player.global_position.distance_to(global_position)
 			if distance_from_grenade <= DAMAGE_RADIUS and "hit" in player:
+				print(distance_from_grenade / DAMAGE_RADIUS)
 				var damage: int = floor(GRENADE_MIN_DAMAGE + (distance_from_grenade / DAMAGE_RADIUS) * (GRENADE_MAX_DAMAGE - GRENADE_MIN_DAMAGE))
 				player.hit(damage, true)
 
