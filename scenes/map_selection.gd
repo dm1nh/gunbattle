9574c539228746142e91b49dbe7e88b08dde32dd
dm1nh <dangminhngo.dev@gmail.com>
2024-07-08 11:11:54 +0700

    fix: show map overview on map selection menu

--- scenes/map_selection.gd
+++ scenes/map_selection.gd
@@ -12,3 +12,12 @@ func _on_factory_button_pressed():
 func _on_mosque_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/maps/mosque.tscn")
 
+
+func _on_forest_button_mouse_entered():
+	$TextureRect.texture = load("res://graphics/interfaces/maps/forest.png")
+
+func _on_factory_button_mouse_entered():
+	$TextureRect.texture = load("res://graphics/interfaces/maps/factory.png")
+
+func _on_mosque_button_mouse_entered():
+	$TextureRect.texture = load("res://graphics/interfaces/maps/mosque.png")
